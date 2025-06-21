8b205c931ce1a9665a45b14c24781050b9e70da1
Killmongers <moolyaswastik48@gmail.com>
2025-06-21 22:42:47 +0530

    add

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-
 
 app = FastAPI()
 
+# Allow your frontend origin
+origins = [
+    "https://portfilio-2.onrender.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/download-pdf")
-def stream_pdf():
+def download_pdf():
     try:
-        pdf_path = "file.pdf"  # e.g., "static/mydoc.pdf"
-        file = open(pdf_path, mode="rb")
+        file = open("file.pdf", "rb")
         return StreamingResponse(file, media_type="application/pdf", headers={
-            "Content-Disposition": 'attachment; filename="file.pdf"'
-            })
-    except FIleNotFoundError:
-        raise HTTPException(status_code=404 ,details="PDF file not Found")
+            "Content-Disposition": 'attachment; filename="My_CV.pdf"'
+        })
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="PDF file not found")
+
